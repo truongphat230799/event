59a41e85b3128ff7a40a93c29fc93581176b3b01
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2022-06-10 14:31:45 +0700

    Update definition.js

--- definition.js
+++ definition.js
@@ -37,5 +37,36 @@ Blockly.Blocks["yolobit_after_second_do"] = {
     var code = "";
     return code;
   };
+  Blockly.Blocks["yolobit_wait_for"] = {
+    init: function() {
+      this.jsonInit({
+        message0: " khi %1 thực hiện %2 %3",
+        args0: [
+          { type: "input_dummy"
+            variable :"điều kiện"},
+          { type: "input_dummy" },
+          {
+            type: "input_statement", 
+            name: "ACTION"
+          }          
+        ],
+        output: null,
+        colour: "#ECCB00"
+      });
+    }
+  };
   
+  Blockly.Python["yolobit_wait_for"] = function(block) {
+    // TODO: Assemble Python into code variable.
+    var statements_action = Blockly.Python.statementToCode(block, 'ACTION');
+    globals = globals.length ? Blockly.Python.INDENT + 'global ' + globals.join(', ') : '';
+    var cbFunctionName = Blockly.Python.provideFunction_(self,
+    ['def ' + Blockly.Python.FUNCTION_NAME_PLACEHOLDER_ + '(' + variable_message + '):',
+      globals,
+      statements_action || Blockly.Python.PASS
+    ]);
+    var code = "";
+    // TODO: Change ORDER_NONE to the correct strength.
+    return code;
+  };
  
